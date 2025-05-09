d0713321e1727925bb90b555d1e677a41112667a
kanishk44 <kanishk.shedsale@gmail.com>
2025-05-09 16:48:15 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
