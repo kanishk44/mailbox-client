10c89c83c4889766489400262da8d886ea9f9310
kanishk44 <kanishk.shedsale@gmail.com>
2025-05-09 17:38:33 +0530

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import dotenv from "dotenv";
 
-dotenv.config();
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd());
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": process.env.VITE_API_URL,
+  return {
+    server: {
+      proxy: {
+        // Only used for development
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
+  };
 });
