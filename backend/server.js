2772a7e312dc545425a4e4d96ea6029c9f541fc8
kanishk44 <kanishk.shedsale@gmail.com>
2025-05-09 16:15:31 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
