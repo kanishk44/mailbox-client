5327eb67d166c6c61268954cad1ab7d84b62cae1
kanishk44 <kanishk.shedsale@gmail.com>
2025-05-09 15:51:49 +0530

    env fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": import.meta.env.VITE_API_URL,
     },
   },
   plugins: [react(), tailwindcss()],
